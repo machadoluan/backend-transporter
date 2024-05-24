ac8161e7fde7b835c73bb175ea1b300135fbf56d
machadoluan <machado.luandealmeida@gmail.com>
2024-05-24 19:03:15 -0300

    deploy

--- config/database.js
+++ config/database.js
@@ -1,9 +1,12 @@
 const mysql = require('mysql2');
+require('dotenv').config()
 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  database: 'email'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+  database: process.env.DB_BASE
 });
 
 connection.connect((err) => {
